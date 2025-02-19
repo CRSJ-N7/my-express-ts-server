73e478eeb0ca2f99e09d0ca672ca9c3156104b88
crossjawz@gmail.com <crossjawz@gmail.com>
2025-02-19 16:31:45 +0300

    added get,post,delete methods

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import todosRouter from '../routes/todos';
 
 const app = express();
 
@@ -10,8 +11,10 @@ app.use(cors({ origin: '*' }));
 const staticDirPath = path.normalize(`${__dirname}/../public`);
 app.use('/public', express.static(staticDirPath));
 
+app.use('/todos', todosRouter);
+
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
+  res.json({ message: 'Stay FUCKING calm!' });
 });
 
 export default app;
