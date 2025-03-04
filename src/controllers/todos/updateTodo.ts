f5e3aacb76dea724ad31519a1fd43a91f1c3a5ea
crossjawz@gmail.com <crossjawz@gmail.com>
2025-03-04 13:16:31 +0300

    added trimmed text feature  to updateTodo method

--- src/controllers/todos/updateTodo.ts
+++ src/controllers/todos/updateTodo.ts
@@ -12,7 +12,12 @@ export const updateTodo = async (req: Request<{ id: string }, unknown, Partial<T
       res.status(404).json({ message: 'Todo not found' });
     } else {
       if (req.body.text !== undefined) {
-        todos[index].text = req.body.text;
+        const trimmedText = req.body.text.replace(/\s+/g, ' ').trim(); // 👹
+        if (!trimmedText) {
+          res.status(400).json({ message: 'Text cannot be empty' });
+          return;
+        }
+        todos[index].text = trimmedText;
       }
       if (req.body.isCompleted !== undefined) {
         todos[index].isCompleted = req.body.isCompleted;
