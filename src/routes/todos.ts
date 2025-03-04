b31252b362f28b5498c6816e3ca5d8e81dc19fd3
crossjawz@gmail.com <crossjawz@gmail.com>
2025-03-04 09:54:30 +0300

    added toggle all statuses  and remove all completed methods

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -3,11 +3,15 @@ import { getTodos } from '../controllers/todos/getTodos';
 import { createTodo } from '../controllers/todos/createTodo';
 import { deleteTodo } from '../controllers/todos/deleteTodo';
 import { updateTodo } from '../controllers/todos/updateTodo';
+import { toggleAllTodos } from '../controllers/todos/toggleAllTodos';
+import { removeAllCompleted } from '../controllers/todos/removeAllCompletedTodos';
 
 const router = express.Router();
 
 router.get('/', getTodos);
 router.post('/', createTodo);
+router.patch('/', toggleAllTodos);
+router.delete('/', removeAllCompleted);
 router.delete('/:id', deleteTodo);
 router.patch('/:id', updateTodo);
 
